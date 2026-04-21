c901433962cfa61c5996dbbe4cb28cf506a9288f
MelColl <melody2.collins@live.uwe.ac.uk>
2026-04-21 23:42:11 +0100

    bug-fixes

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,11 @@
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = './teachablemachine/';
+let imageMode1URL = './teachablemachine/';
+
+let img_1;
+let img_2;
+let img_3;
 
 // Video
 let video;
@@ -9,29 +13,18 @@ let flippedVideo;
 // To store the classification
 let label = "";
 
-//images
-let img_1; 
-let img_2;
-let img_3;
-
-
 
 // Load the model first
 function preload() {
-  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
-  img_1 = loadImage(
-    'images/PikachuState.jpeg',
-    () => console.log("loaded pikachu"),
-    () => console.log("FAILED pikachu")
-   ); // NEW
-  img_2 = loadImage('images/BulbasaurState.jpeg'); // NEW
-  img_3 = loadImage('images/IdleState.jpeg'); // NEW
+  classifier = ml5.imageClassifier(imageMode1URL + 'model.json');
+  img_1 = loadImage('images/pikachu.jpeg'); // NEW
+  img_2 = loadImage('images/bulbasaur.jpeg'); // NEW
+  img_3 = loadImage('images/idle.jpeg'); // NEW
 }
 
 function setup() {
-  console.log("running setup");
   createCanvas(600, 600);
-  imageModelURL(CENTER);
+ // imageModeURL(CENTER);
   // Create the video
   video = createCapture(VIDEO);
   video.size(600, 600);
@@ -44,31 +37,34 @@ function setup() {
 
 function draw() {
   background(0);
-  imageModelURL(img_1, width/2, height/2, 250, 250);
+  imageMode1URL(CORNER);
   // Draw the video
-  //image(flippedVideo, 0, 0);
+  image(flippedVideo, 0, 0);
+  imageMode(CENTER);
+  image(img_1, width/2, height/2, 600, 600);
 
   // Draw the label
-  //fill(255);
-  //textSize(16);
-  //textAlign(CENTER);
-  //text(label, width / 2, height - 4);
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
 
-  //if (label == "bulbasaur") {
-  //image(img_2, width/2, height/2, 250, 250); // NEW
-//}
-//else if (label == "pikachu") {
- // image(img_1, width/2, height/2, 250, 250); // NEW
-//}
-//else {
-  //image(img_3, width/2, height/2, 250, 250); // NEW
-//} 
+  if (label == "bulbasaur") {
+    image(img_2, width/2, height/2, 250, 250); // NEW
+    }
+else if (label == "pikachu") {
+   image(img_1, width/2, height/2, 250, 250); // NEW
+    }
+else  {
+   image(img_3, width/2, height/2, 250, 250); // NEW
+  } 
 }
 
 // Get a prediction for the current video frame
 function classifyVideo() {
   flippedVideo = ml5.flipImage(video)
   classifier.classify(flippedVideo, gotResult);
+  flippedVideo.remove();
 }
 
 // When we get a result
